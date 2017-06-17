99ea0337115fb222154c65a406e56d4dcbb8960c
Bohdan Danishevsky <dbn@aminis.com.ua>
2017-06-17 14:55:18 +0300

    + refactoring for sms-test-for-pi.py + flake8 configuration files for further linting

--- sms-test-for-pi.py
+++ sms-test-for-pi.py
@@ -1,87 +1,92 @@
 ﻿#!/usr/bin/python3
-from test_shared import *
-from lib.sim900.smshandler import SimGsmSmsHandler, SimSmsPduCompiler
-import random
-n=0
-x=0 
-def sms() :
-	print  ("entre phone numbr")
-	n=input()
-	print "enter texte"
-	x=input()
-    COMPORT_NAME            = "/dev/ttyAMA0"
+import logging
 
-#logging levels
+from test_shared import initializeLogs, initializeUartPort, baseOperations
+from lib.sim900.smshandler import SimGsmSmsHandler, SimSmsPduCompiler
+
+
+def printScaPlusPdu(pdu, logger):
+    # printing SCA+PDU just for debug
+    d = pdu.compile()
+    if d is None:
+        return False
+
+    for (sca, pdu, ) in d:
+        logger.info("sendSms(): sca + pdu = \"{0}\"".format(sca + pdu))
+
+
+def sendSms(sms, pdu, logger):
+    # just for debug printing all SCA + PDU parts
+    printScaPlusPdu(pdu, logger)
+
+    if not sms.sendPduMessage(pdu, 1):
+        logger.error("error sending SMS: {0}".format(sms.errorText))
+        return False
+
+    return True
+
+
+def main():
+    """
+    Tests SMS sending.
+
+    :return: true if everything was OK, otherwise returns false
+    """
+
+    print("Please, enter phone number")
+    phone_number = input()
+
+    print("Please, enter sms text: ")
+    sms_text = input()
+
+    # logging levels
     CONSOLE_LOGGER_LEVEL    = logging.INFO
     LOGGER_LEVEL            = logging.INFO
 
-#WARN: scecify recipient number here!!!
-    TARGET_PHONE_NUMBER     =(n)
+    COMPORT_NAME = "/dev/ttyAMA0"
 
-#You can specify SMS center number, but it's not necessary. If you will not specify SMS center number, SIM900
-#module will get SMS center number from memory
-# SMS_CENTER_NUMBER       = "+1 050 123 45 67"
+    # WARN: scecify recipient number here!!!
+    TARGET_PHONE_NUMBER     = phone_number
+
+    # You can specify SMS center number, but it's not necessary. If you will not specify SMS center number, SIM900
+    # module will get SMS center number from memory
+    # SMS_CENTER_NUMBER       = "+1 050 123 45 67"
+
     SMS_CENTER_NUMBER       = ""
 
-    def printScaPlusPdu(pdu, logger):
-    #printing SCA+PDU just for debug
-        d = pdu.compile()
-        if d is None:
-            return False
+    # adding & initializing port object
+    port = initializeUartPort(portName=COMPORT_NAME)
 
-        for (sca, pdu, ) in d:
-            logger.info("sendSms(): sca + pdu = \"{0}\"".format(sca + pdu))
+    # initializing logger
+    (formatter, logger, consoleLogger,) = initializeLogs(LOGGER_LEVEL, CONSOLE_LOGGER_LEVEL)
 
-    def sendSms(sms, pdu, logger):
-    #just for debug printing all SCA + PDU parts
-        printScaPlusPdu(pdu, logger)
+    # making base operations
+    d = baseOperations(port, logger)
+    if d is None:
+        return False
 
-        if not sms.sendPduMessage(pdu, 1):
-            logger.error("error sending SMS: {0}".format(sms.errorText))
-            return False
+    (gsm, imei) = d
 
-        return True
+    # creating object for SMS sending
+    sms = SimGsmSmsHandler(port, logger)
 
-    def main():
-        """
-        Tests SMS sending.
-
-        :return: true if everything was OK, otherwise returns false
-        """
-
-    #adding & initializing port object
-        port = initializeUartPort(portName=COMPORT_NAME)
-
-    #initializing logger
-        (formatter, logger, consoleLogger,) = initializeLogs(LOGGER_LEVEL, CONSOLE_LOGGER_LEVEL)
-
-    #making base operations
-        d = baseOperations(port, logger)
-        if d is None:
-            return False
-
-        (gsm, imei) = d
-
-        #creating object for SMS sending
-        sms = SimGsmSmsHandler(port, logger)
-
-        #ASCII
-        logger.info("sending sms")
-        pduHelper = SimSmsPduCompiler(
-            SMS_CENTER_NUMBER,
-            TARGET_PHONE_NUMBER,
-            (x)
+    # ASCII
+    logger.info("sending sms")
+    pduHelper = SimSmsPduCompiler(
+        SMS_CENTER_NUMBER,
+        TARGET_PHONE_NUMBER,
+        "{}\n{}".format(
+            sms_text,
             "This is a computer do no reply!"
         )
-        if not sendSms(sms, pduHelper, logger):
-            return False
+    )
+    if not sendSms(sms, pduHelper, logger):
+        return False
+
+    gsm.closePort()
+    return True
 
 
-
-        gsm.closePort()
-        return True
-
-    if __name__ == "__main__":
-        main()
-        print("DONE")
-sms()
+if __name__ == "__main__":
+    main()
+    print("DONE")
